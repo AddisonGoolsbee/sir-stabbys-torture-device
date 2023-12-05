256a271d628e825bc84edee702a064c8d89c8b95
Purpur <pemigrade@gmail.com>
2023-12-05 14:13:59 -0500

    victim.py distortions

--- src/victim/victim.py
+++ src/victim/victim.py
@@ -9,6 +9,9 @@ import wave
 import whisper
 import pyaudio
 
+messages = []
+log = []
+
 # import pygame without the welcome message
 with open(os.devnull, 'w') as f:
     old_stdout = sys.stdout
@@ -44,7 +47,6 @@ def text_to_speech(text_input: str):
 
 
 def record_audio():
-  
     # choose from a list a random speak now
     text_to_speech("You may now speak! You have 10 seconds...")
     stream = audio.open(
@@ -58,13 +60,13 @@ def record_audio():
 
     frames = []
 
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    for _ in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
         data = stream.read(CHUNK)
         frames.append(data)
 
     stream.stop_stream()
     # choose from a list some random silence
-    text_to_speech("SILENCE YOU BOOT LICKER!!!")
+    text_to_speech("SILENCE YOU Poopy McPoopface...!!!")
     stream.close()
     audio.terminate()
 
@@ -75,10 +77,36 @@ def record_audio():
     waveFile.writeframes(b''.join(frames))
     waveFile.close()
     result = model.transcribe("output.wav", fp16=False)
-    print(result)
+    print(result["text"])
+    return result["text"]
 
-def distort_output(text_input: str):
-    pass
+
+messages_2 = [
+    {"role": "system", "content": "You are behaving as a middle-man to distort communications between two people. You will receive a message and make the message be a little scrambled but still somewhat understandable when spoken using Syllable Swap. An example is: Original: \"This is an example.\" Syllable Swap: \"Isth is na exampel.\" It should still be somewhat understandable, so don't make it too severe!"},
+    {"role": "user", "content": "I'll believe you if you can tell me what your name is at the very least."},
+    {"role": "assistant", "content": "Ill belive you fi you anc tell me whay our name es at e veryth least."}
+]
+
+def distort_agent_input(user_input: str):
+    global log
+    # add original agent input to log, then
+    # distort the output using singular distortion model
+    # add the distorted output to log
+    # speak distorted output
+    log += f"Agent: {user_input}\n"
+    messages_2.append({"role": "user", "content": user_input})
+    # use gpt-4-turbo
+    completion = client.chat.completions.create(
+        model="gpt-4-1106-preview",
+        temperature=1.0,
+        messages=messages_2
+    )
+    content_obj = completion.choices[0].message.content
+    print(content_obj)
+    log += f"Agent (DISTORTED): {content_obj}\n"
+    messages.append({"role": "assistant", "content": content_obj})
+    text_to_speech(content_obj)
+    return content_obj
   
 if __name__ == '__main__':
     load_dotenv()
@@ -101,7 +129,8 @@ if __name__ == '__main__':
     client = openai.OpenAI()
     try:
         # victimLoop()
-        record_audio()
+        text = record_audio()
+        distort_agent_input(text)
     except KeyboardInterrupt:
         print('\nAgent exited')
     except Exception as e:
