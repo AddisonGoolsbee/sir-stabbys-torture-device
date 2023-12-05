ace77d7658ab7d92428dd610bed82c5cee79623e
Purpur <pemigrade@gmail.com>
2023-12-04 23:01:46 -0500

    merge

--- src/victim/test.py
+++ src/victim/test.py
@@ -1,51 +1,10 @@
 #brew install portaudio
 
-import pyaudio
-import wave
-import whisper
 
-p = pyaudio.PyAudio()
-# for i in range(p.get_device_count()):
-#     dev = p.get_device_info_by_index(i)
-#     print(f"{i}: {dev['name']}")
 
-def record_audio():
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 16000
-    CHUNK = 1024
-    RECORD_SECONDS = 10
-    WAVE_OUTPUT_FILENAME = "output.wav"
 
-    audio = pyaudio.PyAudio()
 
-    stream = audio.open(
-        format=FORMAT,
-        channels=CHANNELS,
-        rate=RATE,
-        input=True,
-        frames_per_buffer=CHUNK,
-        input_device_index=1
-    )
 
-    frames = []
-
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
-
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
-
-    waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    waveFile.setnchannels(CHANNELS)
-    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-    waveFile.setframerate(RATE)
-    waveFile.writeframes(b''.join(frames))
-    waveFile.close()
-
-model = whisper.load_model("base")
 
 # capture audio once
 print("start")
@@ -53,7 +12,3 @@ record_audio()
 
 print("finish")
 
-model = whisper.load_model("base")
-
-result = model.transcribe("output.wav")
-print(result)
